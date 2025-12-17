39e9dfcd85b947a024735ff51791f4627afc4fee
Yashraj Chouhan <your-github-email@gmail.com>
2025-12-17 12:35:45 +0530

    [Yashraj Chouhan] : Added a program file to calculate Sam's Average Marks

---
+++ gcr-codebase/programming-elements/Level-1/SamAverageMarks.java
@@ -0,0 +1,22 @@
+// Create a class Sam'sAverageMarks to calculate the average marks of Sam in PCM.
+
+class SamAverageMarks{
+public static void main(String args[])
+{
+//Create a variable mathsMarks indicates Sam's Marks in Maths
+ int mathsMarks=94;
+
+//Create a variable physicsMarks indicates Sam's Marks in Physics.
+ int physicsMarks=95;
+ 
+//Create a variable chemistryMarks indicates Sam's Marks in Chemistry.
+ int chemistryMarks=96;
+ 
+//Create a variable averageMarks to calculate Average Marks of Sam. 
+ int averageMarks=(mathsMarks+physicsMarks+chemistryMarks)/3;
+ 
+ //Print the Average Marks of Sam.
+ System.out.println("Sam's average mark in PCM is" +" " + averageMarks);
+}
+
+}
