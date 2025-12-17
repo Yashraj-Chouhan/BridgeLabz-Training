440916924a4820a92ce008a8128304b544b0cea3
Yashraj Chouhan <your-github-email@gmail.com>
2025-12-17 11:58:24 +0530

    [Yashraj Chouhan] : Added a folder for Level-1 Programming concepts:

---
+++ gcr-codebase/programming-elements/Level-1/HarryAge.java
@@ -0,0 +1,18 @@
+//Create HarryAge class to calculate the current age of Harry.
+
+class HarryAge{
+ public static void main(String args[])
+ {
+//Create a variable birthYear indicate the Year of birth of Harry.
+   int birthYear=2000;
+   
+// Create a variable currentYear indicates the current running year.
+   int currentYear=2024;
+   
+//Create the variable age indicates the actual age of haarry. 
+   int age=currentYear-birthYear;
+   
+//Print the Age of Harry
+   System.out.println("Harry's age in 2024 is" + age);
+ }
+}
