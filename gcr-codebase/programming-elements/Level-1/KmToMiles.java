e1a50d9a7eb1d63b96b5b0cfaea02fa8a75da2f5
Yashraj Chouhan <your-github-email@gmail.com>
2025-12-17 12:52:26 +0530

    [Yashraj Chouhan] : Added a program file for converting KM to Miles

---
+++ gcr-codebase/programming-elements/Level-1/KmToMiles.java
@@ -0,0 +1,17 @@
+ //Create a class KmToMiles to Convert The Disatnace in KM into Distance into MIles.
+ public class KmToMiles{
+ public static void main(String args[])
+ {
+ // Create a variable distanceInKM indicates distance in KM.
+   double distanceInKM=10.8;
+   
+ // Create a variable distanceInMiles indicates Convertes distance Measure in miles.
+   double distanceInMiles=10.8/1.6;
+   
+ // Print the Converted Km to Miles Disatnace.
+   System.out.println("The distance" +" "+distanceInKM+" "+ 
+   "km in miles is" +" "+ distanceInMiles);
+
+ }
+ 
+}
