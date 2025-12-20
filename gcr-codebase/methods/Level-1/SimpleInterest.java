3477d066f3ee5af642d6722f4f9302e197364aa8
Yashraj Chouhan <your-github-email@gmail.com>
2025-12-20 12:18:52 +0530

    [Yashraj Chouhan] : Simple Interest Calculator

---
+++ gcr-codebase/methods/Level-1/SimpleInterest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SimpleInterest{
+	public static double simpleInterest(int principal,double rate,double time ){
+		double simpleInterest = (double) principal* time/100;
+		return simpleInterest;
+	}
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int principal=sc.nextInt();
+		double rate=sc.nextDouble();
+		double time=sc.nextDouble();
+		
+		System.out.println("The Simple Interest is"+ simpleInterest(principal,rate,time)+" for Principal "+principal+
+		                    " Rate of Interest "+ rate + " and Time ");
+	}
+}
