ccf720d9288b4b62ed3d458cf43f6a879d0d8aa1
Yashraj Chouhan <your-github-email@gmail.com>
2025-12-21 21:37:22 +0530

    [Yashraj Chouhan] Factor Operation

---
+++ gcr-codebase/methods/Level-2/FactorsOperation.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class FactorsOperations {
+
+    // Method to find factors and return array
+    static int[] findFactors(int number) {
+        int count = 0;
+
+        // First loop to count factors
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+
+        // Second loop to store factors
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+
+        return factors;
+    }
+
+    // Method to find sum of factors
+    static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += factor;
+        }
+        return sum;
+    }
+
+    // Method to find product of factors
+    static long productOfFactors(int[] factors) {
+        long product = 1;
+        for (int factor : factors) {
+            product *= factor;
+        }
+        return product;
+    }
+
+    // Method to find sum of squares of factors
+    static double sumOfSquares(int[] factors) {
+        double sum = 0;
+        for (int factor : factors) {
+            sum += Math.pow(factor, 2);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int[] factors = findFactors(number);
+
+        System.out.println("Factors:");
+        for (int factor : factors) {
+            System.out.print(factor + " ");
+        }
+
+        System.out.println("\nSum of factors: " + sumOfFactors(factors));
+        System.out.println("Product of factors: " + productOfFactors(factors));
+        System.out.println("Sum of squares of factors: " + sumOfSquares(factors));
+
+        sc.close();
+    }
+}
