a664da0333daffc766990ce7333ef00d3863b042
Yashraj Chouhan <your-github-email@gmail.com>
2025-12-22 22:55:05 +0530

    [Yashraj Chouhan] Student Grade Card

---
+++ gcr-codebase/strings/Level-2/StudentGradeCard.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class StudentGradeCard {
+
+    // Method to generate random PCM scores
+    public static int[][] generateScores(int n) {
+        int[][] scores = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            scores[i][0] = (int)(Math.random() * 90) + 10; // Physics
+            scores[i][1] = (int)(Math.random() * 90) + 10; // Chemistry
+            scores[i][2] = (int)(Math.random() * 90) + 10; // Maths
+        }
+        return scores;
+    }
+
+    // Method to calculate total, average, percentage
+    public static double[][] calculateResult(int[][] scores) {
+        double[][] result = new double[scores.length][3];
+
+        for (int i = 0; i < scores.length; i++) {
+            int total = scores[i][0] + scores[i][1] + scores[i][2];
+            double average = total / 3.0;
+            double percentage = (total / 300.0) * 100;
+
+            result[i][0] = total;
+            result[i][1] = Math.round(average * 100.0) / 100.0;
+            result[i][2] = Math.round(percentage * 100.0) / 100.0;
+        }
+        return result;
+    }
+
+    // Method to calculate grade
+    public static String[] calculateGrade(double[][] result) {
+        String[] grade = new String[result.length];
+
+        for (int i = 0; i < result.length; i++) {
+            double percent = result[i][2];
+
+            if (percent >= 80)
+                grade[i] = "A";
+            else if (percent >= 70)
+                grade[i] = "B";
+            else if (percent >= 60)
+                grade[i] = "C";
+            else if (percent >= 50)
+                grade[i] = "D";
+            else if (percent >= 40)
+                grade[i] = "E";
+            else
+                grade[i] = "R";
+        }
+        return grade;
+    }
+
+    // Method to display scorecard
+    public static void display(int[][] scores, double[][] result, String[] grade) {
+        System.out.println("\nPhy\tChem\tMath\tTotal\tAvg\t%\tGrade");
+        System.out.println("--------------------------------------------------");
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(
+                scores[i][0] + "\t" +
+                scores[i][1] + "\t" +
+                scores[i][2] + "\t" +
+                (int)result[i][0] + "\t" +
+                result[i][1] + "\t" +
+                result[i][2] + "\t" +
+                grade[i]
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[][] scores = generateScores(n);
+        double[][] result = calculateResult(scores);
+        String[] grade = calculateGrade(result);
+
+        display(scores, result, grade);
+    }
+}
+
