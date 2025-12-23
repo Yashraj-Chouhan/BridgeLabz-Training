29952c2a977cc42774a32a4e19397e59bd811db0
Yashraj Chouhan <your-github-email@gmail.com>
2025-12-23 22:35:09 +0530

    [Yashraj Chouhan] : ToggleCase.java

---
+++ gcr-codebase/Extras/Extra-String/ToggleCase.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ToggleCase {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                result += (char)(ch + 32);
+            } 
+            else if (ch >= 'a' && ch <= 'z') {
+                result += (char)(ch - 32);
+            } 
+            else {
+                result += ch;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
