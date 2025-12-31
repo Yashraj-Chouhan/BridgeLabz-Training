ba857721cf6d69a66b4f69a530917519b07d305a
Yashraj Chouhan <your-github-email@gmail.com>
2025-12-31 16:23:27 +0530

    [Yashraj Chouhan] Bus Route Distance Tracker

---
+++ scenario-based/BusRouteDistanceTracker.java
@@ -0,0 +1,35 @@
+/*19. Bus Route Distance Tracker 🚌
+Each stop adds distance.
+● Ask if the passenger wants to get off at a stop.
+● Use a while-loop with a total distance tracker.
+● Exit on user confirmation.
+*/
+import java.util.Scanner;
+
+public class BusRouteDistanceTracker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double totalDistance = 0;
+        char choice = 'n';
+        while (true) {
+            System.out.print("Enter distance covered from this stop (km): ");
+            double distance = sc.nextDouble();
+
+            totalDistance += distance;
+
+            System.out.println("Total distance covered: " + totalDistance + " km");
+
+            System.out.print("Do you want to get off at this stop? (y/n): ");
+            choice = sc.next().charAt(0);
+
+            if (choice == 'y' || choice == 'Y') {
+                break;
+            }
+        }
+
+        System.out.println("Passenger exited the bus.");
+        System.out.println("Final distance travelled: " + totalDistance + " km");
+
+        sc.close();
+    }
+}
