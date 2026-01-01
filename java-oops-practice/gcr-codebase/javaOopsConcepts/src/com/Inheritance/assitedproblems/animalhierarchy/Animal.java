34db51efd8c5b9b5c81a44c418bbbe74851c6b62
Yashraj Chouhan <your-github-email@gmail.com>
2026-01-01 17:17:19 +0530

    [Yashraj Chouhan] Animal Hierarchy

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/Inheritance/assitedproblems/animalhierarchy/Animal.java
@@ -0,0 +1,15 @@
+package com.Inheritance.assitedproblems.animalhierarchy;
+
+public class Animal {
+    String name;
+    int age;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
