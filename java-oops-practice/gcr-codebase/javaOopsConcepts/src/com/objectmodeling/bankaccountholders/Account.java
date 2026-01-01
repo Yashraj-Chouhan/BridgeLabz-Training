236ba590044a09e469d4b123c84fe29f45a4208d
Yashraj Chouhan <your-github-email@gmail.com>
2026-01-01 12:11:50 +0530

    [Yashraj Chouhan] Account details

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/objectmodeling/bankaccountholders/Account.java
@@ -0,0 +1,20 @@
+package com.objectmodeling.bankaccountholders;
+
+public class Account {
+    private int accountNumber;
+    private double balance;
+
+    public Account(int accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
