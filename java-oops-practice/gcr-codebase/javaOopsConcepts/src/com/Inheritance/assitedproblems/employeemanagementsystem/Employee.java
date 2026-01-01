ad9e133a309df4ccbf344bc485dc3779a0cc06eb
Yashraj Chouhan <your-github-email@gmail.com>
2026-01-01 17:19:04 +0530

    [Yashraj Chouhan] Employee Management System

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/Inheritance/assitedproblems/employeemanagementsystem/Employee.java
@@ -0,0 +1,20 @@
+package com.Inheritance.assitedproblems.employeemanagementsystem;
+
+public class Employee {
+    String name;
+    int id;
+    double salary;
+
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
