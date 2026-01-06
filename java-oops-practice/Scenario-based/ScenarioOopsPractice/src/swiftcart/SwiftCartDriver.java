66c919c4a8a5c57c3f9d6e9e1605f5ef03f3d09f
Yashraj Chouhan <your-github-email@gmail.com>
2026-01-06 14:20:42 +0530

    [Yashraj Chouhan] : Updated SwiftCartDriver logic

--- java-oops-practice/Scenario-based/ScenarioOopsPractice/src/swiftcart/SwiftCartDriver.java
+++ java-oops-practice/Scenario-based/ScenarioOopsPractice/src/swiftcart/SwiftCartDriver.java
@@ -1,5 +1,16 @@
 package swiftcart;
 
 public class SwiftCartDriver {
+    public static void main(String[] args) {
 
+        Product milk = new PerishableProduct("Milk", 50);
+        Product rice = new NonPerishableProduct("Rice", 60);
+
+        Cart cart = new Cart();
+        cart.addProduct(milk, 2);   // price * quantity
+        cart.addProduct(rice, 3);
+
+        cart.applyDiscount();
+        cart.generateBill();
+    }
 }
