9e10e6893074d347d737c8dfe826e28d6155f6ce
Yashraj Chouhan <your-github-email@gmail.com>
2026-01-06 16:19:14 +0530

    [Yashraj Chouhan] queue Using Stack

--- java-dsa-practice/gcr-codebase/dsapractice/src/stacks/queueusingstack/QueueDriver.java
+++ java-dsa-practice/gcr-codebase/dsapractice/src/stacks/queueusingstack/QueueDriver.java
@@ -1,5 +1,16 @@
 package stacks.queueusingstack;
 
 public class QueueDriver {
+    public static void main(String[] args) {
 
+        QueueUsingStacks queue = new QueueUsingStacks();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+
+        System.out.println("Dequeued: " + queue.dequeue());
+        System.out.println("Front: " + queue.peek());
+        System.out.println("Dequeued: " + queue.dequeue());
+    }
 }
--- java-dsa-practice/gcr-codebase/dsapractice/src/stacks/queueusingstack/QueueUsingStacks.java
+++ java-dsa-practice/gcr-codebase/dsapractice/src/stacks/queueusingstack/QueueUsingStacks.java
@@ -1,5 +1,55 @@
 package stacks.queueusingstack;
 
+import java.util.Stack;
 public class QueueUsingStacks {
 
+    private Stack<Integer> stackEnqueue;
+    private Stack<Integer> stackDequeue;
+
+    // Constructor
+    public QueueUsingStacks() {
+        stackEnqueue = new Stack<>();
+        stackDequeue = new Stack<>();
+    }
+
+    // Enqueue operation
+    public void enqueue(int data) {
+        stackEnqueue.push(data);
+        System.out.println(data + " enqueued");
+    }
+
+    // Dequeue operation
+    public int dequeue() {
+
+        if (stackDequeue.isEmpty()) {
+
+            // Transfer elements only when needed
+            while (!stackEnqueue.isEmpty()) {
+                stackDequeue.push(stackEnqueue.pop());
+            }
+        }
+
+        if (stackDequeue.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        return stackDequeue.pop();
+    }
+
+    // Display front element
+    public int peek() {
+        if (stackDequeue.isEmpty()) {
+            while (!stackEnqueue.isEmpty()) {
+                stackDequeue.push(stackEnqueue.pop());
+            }
+        }
+
+        if (stackDequeue.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        return stackDequeue.peek();
+    }
 }
