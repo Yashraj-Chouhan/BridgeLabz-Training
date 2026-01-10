c3ad97fba11e411320a96386b118ff8757a24090
Yashraj Chouhan <your-github-email@gmail.com>
2026-01-10 11:21:01 +0530

    [Yashraj Chouhan] Tour Mate App

---
+++ java-oops-practice/Scenario-based/ScenarioOopsPractice/src/tourmate/Trip.java
@@ -0,0 +1,16 @@
+package tourmate;
+
+public class Trip {
+	String destination;
+	int budget;
+	int duration;
+	
+	public Trip(String destination, int budget, int duration) {
+		super();
+		this.destination = destination;
+		this.budget = budget;
+		this.duration = duration;
+	}
+	
+
+}
